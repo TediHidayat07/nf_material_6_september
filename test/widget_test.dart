739fb4b2df9fdce5afb984968b8ae552ae54cdac
Tedi Hidayat <tedihidayat198@gmail.com>
2022-09-17 08:48:43 +0700

    perbaikan import pada file widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:latihan_nf/main.dart';
+import 'package:nf_material_6_september/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
